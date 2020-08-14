c222fb226c9ea4bee856159c67c01f35f95ddaf6
Yihui Wang <jameswangyh@gmail.com>
2020-08-14 16:19:07 -0400

    publish

---
+++ TanksAI/Assets/AI/Scripts/LookDecision.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TANKS;
+
+[CreateAssetMenu(menuName = "PluggableAI/Decisions/Look")]
+public class LookDecision : Decision
+{
+    public override bool Decide(StateController controller)
+    {
+        bool targetVisible = Look(controller);
+        return targetVisible;
+    }
+
+    private bool Look(StateController controller)
+    {
+        RaycastHit hit;
+        ShellExplosion shellExplosion;
+        Debug.DrawRay(controller.eyes.position, controller.eyes.forward.normalized * controller.enemyStats.lookRange, Color.green);
+
+        if (Physics.SphereCast(controller.eyes.position, controller.enemyStats.lookSphereCastRadius, controller.eyes.forward, out hit, controller.enemyStats.lookRange)
+            && hit.collider.CompareTag("Player"))
+        {
+            controller.chaseTarget = hit.transform;
+            controller.stateFlag = true;
+            controller.navMeshAgent.speed = 5;
+            return true;
+        }
+        else if (controller.stateFlag)
+        {
+            controller.stateFlag = !controller.CheckIfCountDownElapsed(controller.enemyStats.searchDuration);
+            if(controller.CheckIfCountDownElapsed(controller.enemyStats.searchDuration))
+                controller.navMeshAgent.speed = 3.5f;
+            return !controller.CheckIfCountDownElapsed(controller.enemyStats.searchDuration);
+        }
+        else
+        {
+            controller.navMeshAgent.speed = 3.5f;
+            return false;
+        }
+    }
+}
---
+++ TanksAI/Assets/Tanks/Assets/Scripts/UI/UIDirectionControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace TANKS
+{
+    public class UIDirectionControl : MonoBehaviour
+    {
+
+        public bool m_UseRelativeRotation = true; 
+
+
+        private Quaternion m_RelativeRotation; 
+
+
+        private void Start ()
+        {
+            m_RelativeRotation = transform.parent.localRotation;
+        }
+
+
+        private void Update ()
+        {
+            if (m_UseRelativeRotation)
+                transform.rotation = m_RelativeRotation;
+        }
+    }
+}
